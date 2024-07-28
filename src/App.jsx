c0f7c6052a2c8974d295d2fb31483d4af4eb7503
sachin <sachinacharya901@gmail.com>
2024-07-28 09:33:48 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1 className="text-3xl font-bold underline bg-red-500 ">
+        Hello world!
+      </h1>
+
+    </>
+  )
+}
+
+export default App
